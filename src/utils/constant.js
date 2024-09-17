1e6d9b9b0098b25a331a51953e37283f64dbaa6b
Sharmila Kumaravel <153075675+22-sharmi@users.noreply.github.com>
2024-09-17 13:24:46 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,7 +3,7 @@
 // export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
 // export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
 
-export const USER_API_END_POINT="https://job-portal-backend-vqw3.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="https://job-portal-backend-vqw3.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://job-portal-backend-vqw3.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://job-portal-backend-vqw3.onrender.com/api/v1/company";
+export const USER_API_END_POINT="https://backend-job-portal-cc5p.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://backend-job-portal-cc5p.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://backend-job-portal-cc5p.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://backend-job-portal-cc5p.onrender.com/api/v1/company";
